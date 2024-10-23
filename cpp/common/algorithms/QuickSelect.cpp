ab8b5f6f3aec9550702f4d9fa3a021fd512a621d
Lefan Hu <lefanhu1@gmail.com>
2024-10-22 23:08:28 -0400

    quick select

--- cpp/common/algorithms/QuickSelect.cpp
+++ cpp/common/algorithms/QuickSelect.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 int quick_select(vector<int>& v, int k) {
-        vector<int> low, mid, high;
+    vector<int> low, mid, high;
 
-        int piv = v[v.size() / 2];
-        for (auto& n : v) {
-            if (n < piv) {
-                low.push_back(n);
-            } else if (n > piv) {
-                high.push_back(n);
-            } else {
-                mid.push_back(n);
-            }
-        }
-
-        // divide & conquer
-        if (k <= high.size()) {
-            return quick_select(high, k);
-        } else if (k <= high.size() + mid.size()) {
-            return piv;
+    int piv = v[v.size() / 2];
+    for (auto& n : v) {
+        if (n < piv) {
+            low.push_back(n);
+        } else if (n > piv) {
+            high.push_back(n);
         } else {
-            return quick_select(low, k - high.size() - mid.size());
+            mid.push_back(n);
         }
     }
+
+    // divide & conquer
+    if (k <= high.size()) {
+        return quick_select(high, k);
+    } else if (k <= high.size() + mid.size()) {
+        return piv;
+    } else {
+        return quick_select(low, k - high.size() - mid.size());
+    }
+}
